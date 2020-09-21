97deb3f0569544c4685517c9e2ef56d7f19c349f
Pedro <pedrogallardorobinson@gmail.com>
2020-09-21 18:20:06 -0300

    lol

--- TestServerClient/client.c
+++ TestServerClient/client.c
@@ -10,16 +10,11 @@ void func(int sockfd)
 { 
 	char buff[MAX]; 
 	int n; 
-	for (;;) { 
-		bzero(buff, sizeof(buff)); 
-		printf("Enter the string : "); 
-		n = 0; 
-		while ((buff[n++] = getchar()) != '\n') 
-			; 
-		write(sockfd, buff, sizeof(buff)); 
+    while (1) { 
 		bzero(buff, sizeof(buff)); 
 		read(sockfd, buff, sizeof(buff)); 
 		printf("From Server : %s", buff); 
+
 		if ((strncmp(buff, "exit", 4)) == 0) { 
 			printf("Client Exit...\n"); 
 			break; 
@@ -32,7 +27,6 @@ int main()
 	int sockfd, connfd; 
 	struct sockaddr_in servaddr, cli; 
 
-	// socket create and varification 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
 	if (sockfd == -1) { 
 		printf("socket creation failed...\n"); 
@@ -42,12 +36,10 @@ int main()
 		printf("Socket successfully created..\n"); 
 	bzero(&servaddr, sizeof(servaddr)); 
 
-	// assign IP, PORT 
 	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+	servaddr.sin_addr.s_addr = inet_addr("192.168.0.29"); 
 	servaddr.sin_port = htons(PORT); 
 
-	// connect the client socket to server socket 
 	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
 		printf("connection with the server failed...\n"); 
 		exit(0); 
@@ -55,9 +47,6 @@ int main()
 	else
 		printf("connected to the server..\n"); 
 
-	// function for chat 
 	func(sockfd); 
-
-	// close the socket 
 	close(sockfd); 
 } 
